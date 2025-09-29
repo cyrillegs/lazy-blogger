43bdb38fdb95eb8ac7867b2be61c5ccc6921821f
Cyril Dave Legaspi <cyrildave.legaspi@gmail.com>
2025-09-29 12:50:59 +0800

    clean build and fix not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+      <main className="flex h-screen flex-col items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-gray-800">404</h1>
+        <p className="mt-2 text-lg text-gray-600">Page not found</p>
+      </main>
+    );
+  }
+  
